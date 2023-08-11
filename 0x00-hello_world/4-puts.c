909b27993ef4b0a9bb0d1cf97259ab7b5abfda5b
Ebubechukwu2023 <ebubechukwueleh@gmail.com>
2023-08-10 21:49:08 -0700

    5-printf.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#insert <stdio.h> 
+/**
+ * main - programs that can print lines with put function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
+		
