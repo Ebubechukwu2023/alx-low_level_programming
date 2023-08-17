8b586a197ba53da4ec624a42c7fc5b44dab9cf4c
Ebubechukwu2023 <ebubechukwueleh@gmail.com>
2023-08-16 18:08:27 -0700

    wahala

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,10 +16,10 @@ void times_table(void)
 	for (y = 0; y <= 9; y++)
 	{
 	z = x * y;
-	if (z >9)
+	if (z > 9)
 	{
 	u = z % 10;
-	d = (z = u) / 10;
+	d = (z - u) / 10;
 	_putchar(44);
 	_putchar(32);
 	_putchar(d + '0');
@@ -33,7 +33,7 @@ void times_table(void)
 	_putchar(32);
 	_putchar(32);
 	}
-	_putchar(z * '0');
+	_putchar(z + '0');
 	}
 	}
 	_putchar('\n');
